17f18c488feeb70dbe62d55aba38d62574a62604
Dev Halai <dev.h@hotmail.co.uk>
2024-07-31 19:30:42 +0100

    delete unused files and changed taskform location

--- client-react/src/components/LogInCheck.jsx
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-import { useContext } from 'react'
-import { AuthContext } from './Authorisation/AuthProvider'
-import { useNavigate } from 'react-router-dom'
-
-const api = axios.create({baseURL: 'http://localhost:3000'})
--- client-react/src/pages/Home.jsx
+++ client-react/src/pages/Home.jsx
@@ -6,6 +6,11 @@ export default function Home(){
             <h1>Welcome to TaskWizard!</h1>
             <h2>The easiest way to manage all your needs,</h2>
             <h2>And get them done on time!</h2>
+            <div>
+                <div>
+                    
+                </div>
+            </div>
         </div>
     )
 }
