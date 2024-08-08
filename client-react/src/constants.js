8ad2e95c1da1f46c0ac3b44c881295b0307116e5
dev <dev.h@hotmail.co.uk>
2024-08-09 00:28:05 +0100

    Fix API_URL in constants.js

--- client-react/src/constants.js
+++ client-react/src/constants.js
@@ -1,1 +1,3 @@
-export const API_URL = process.env.NODE_ENV === "test" ? "https://www.coolwebsite.com" : import.meta.env.VITE_API_URL
+export const API_URL = process.env.NODE_ENV === "production"
+    ? import.meta.env.VITE_API_URL
+    : "http://localhost:3000/api/v1"
