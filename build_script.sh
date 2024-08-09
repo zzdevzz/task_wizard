8831fa8f04078386ecb4c0f06609e2e5716cda4e
dev <dev.h@hotmail.co.uk>
2024-08-09 23:55:42 +0100

    build file has correct path

--- build_script.sh
+++ build_script.sh
@@ -5,5 +5,5 @@ nvm install 22  # Install Node.js version 22 if not already installed
 nvm use 22  # Switch to Node.js version 22
 npm install
 npm run build
-cp -r public/* ../public/
+cp -r dist/* ../public/
 cd ..
