0b86338502c331b742526b2aa8c5c8c4e056b637
dev <dev.h@hotmail.co.uk>
2024-08-10 00:26:14 +0100

    heroku deployment and debugging

--- client-react/src/utils/api.jsx
+++ client-react/src/utils/api.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import { API_URL, BASE_URL } from '../constants'
 
-console.log("axios file base ran updated NEW")
 console.log("build file running successfully")
 
 console.log('BASE_URL: ', BASE_URL)
