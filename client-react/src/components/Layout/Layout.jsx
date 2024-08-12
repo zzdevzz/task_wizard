3aeef49cd9d452d0b5a3b261437c27119a8ed508
dev <dev.h@hotmail.co.uk>
2024-08-12 23:57:26 +0100

    prelist made for new users

--- client-react/src/components/Layout/Layout.jsx
+++ client-react/src/components/Layout/Layout.jsx
@@ -18,7 +18,7 @@ export default function Layout(){
                         <Outlet/>
                     </div>
                   </div>
-                {/* <Footer/> */}
+                <Footer/>
             </div>
         </div>
     )
