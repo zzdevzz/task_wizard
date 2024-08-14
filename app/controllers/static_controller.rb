27f347c4a7a1cd0b428d59b2a0d0a9f3d488e84e
Dev Halai <dev.h@hotmail.co.uk>
2024-08-14 15:53:31 +0100

    old changes

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,10 +2,6 @@ class StaticController < ApplicationController
   def index
     html_content = File.read(Rails.root.join('public', 'index.html'))
     render html: html_content.html_safe
-    puts "BASE_URL is: #{ENV['BASE_URL']}"
-    puts "VITE_API_URL is: #{ENV['VITE_API_URL']}"
-    puts "NODE ENV is: #{ENV['NODE_ENV']}"
-    puts "STATIC CONTROLLER HERE!"
   end
 
 end
--- client-react/src/components/Tasks/TaskPreview.jsx
+++ client-react/src/components/Tasks/TaskPreview.jsx
@@ -3,7 +3,6 @@ import React from "react"
 export default function TaskPreview({taskData}){
     const { name, description} = taskData
     return (
-
         <li className="task-preview list-group-item bg-transparent glass-container">
             <h2 className="text-truncate">{name}</h2>
             <p className="text-truncate">{description}</p>
