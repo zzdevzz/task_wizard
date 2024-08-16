d69f4ec0e3c2ef0dd476ea613f53a15ddf30fad9
Dev Halai <dev.h@hotmail.co.uk>
2024-08-16 18:00:25 +0100

    constants js edited

--- client-react/src/constants.js
+++ client-react/src/constants.js
@@ -1,20 +1,21 @@
 // EVERYTHING IN THIS FILE NEEDS TO BE THE BACKEND PORT (3000) OR PRODUCTION URL. ALL ADDRSSED TO BE FROM WINDOWS IPCONFIG.
 // THIS IS IPCONFIG FROM WINDOWS.
 
-const dynamicIp = "http://192.168.1.172:3000"
+// const dynamicIp = "http://192.168.1.172:3000"
 
-export const API_URL = process.env.MODE === "production"
-  ? import.meta.env.VITE_API_URL
-  : `${dynamicIp}/api/v1`
+// export const API_URL = process.env.MODE === "production"
+//   ? import.meta.env.VITE_API_URL
+//   : `${dynamicIp}/api/v1`
 
-export const BASE_URL = process.env.MODE === "production"
-  ? import.meta.env.VITE_BASE_URL
-  : `${dynamicIp}`
-
-
-// const API_URL = import.meta.env.VITE_API_URL
+// export const BASE_URL = process.env.MODE === "production"
+//   ? import.meta.env.VITE_BASE_URL
+//   : `${dynamicIp}`
 
 
-// const BASE_URL = import.meta.env.VITE_BASE_URL
+const API_URL = import.meta.env.VITE_API_URL
 
 
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
+
+export {API_URL, BASE_URL}
